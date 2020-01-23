2cf75e955c64da153254dced30f34ff1d4ec8444
avawda <ahmed.vawda@gmail.com>
2020-01-23 21:31:36 +0200

    setup new folder structure

---
+++ 1- The Journey Begins/1.1 add.py
@@ -0,0 +1,18 @@
+# Easy
+
+# Write a function that returns the sum of two numbers.
+
+# Example
+
+# For param1 = 1 and param2 = 2, the output should be
+# add(param1, param2) = 3
+
+
+def add(param1, param2):
+    return param1+param2
+
+
+v1 = 6
+v2 = 7
+
+print(add(v1, v2))
