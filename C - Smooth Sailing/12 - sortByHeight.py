b64af1ec06f5dc278ec0bd3b3a11977006b59466
avawda <ahmed.vawda@gmail.com>
2020-02-04 11:39:27 +0200

    Code Cleanup

--- C - Smooth Sailing/12 - sortByHeight.py
+++ C - Smooth Sailing/12 - sortByHeight.py
@@ -1,3 +1,5 @@
+# EASY
+
 # Some people are standing in a row in a park. There are trees between them which cannot be moved.
 # Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
 
@@ -18,30 +20,33 @@
 #     1 ≤ a.length ≤ 1000,
 #     -1 ≤ a[i] ≤ 1000.
 
-#     [output] array.integer
-#         Sorted array a with all the trees untouched.
+
+def sortByHeight_OPTIMAL(a):
+
+    l = sorted([i for i in a if i > 0])
+    for n,i in enumerate(a):
+        if i == -1:
+            l.insert(n,i)
+    return l
 
 
 def sortByHeight(a):
-    treeLocations = []
-    personHeight = []
-    answer = []
-    for idx, element in enumerate(a):
-        if element == -1:
-            treeLocations.append(idx)
-        else:
-            personHeight.append(element)
+    heights = []
+    # Extract the heights of people and then sort
+    for i in a:
+        if i > 0:
+            heights.append(i)
+    heights.sort()
 
-    personHeight.sort()
-    personPos = 0
-    for i in range(len(a)):
-        if i in treeLocations:
-            answer.append(-1)
-        else:
-            answer.append(personHeight[personPos])
-            personPos += 1
-    return answer
+    # Go through the list and replace peoples heights with the ordered versions
+    output = a.copy()
+    pos = 0
+    for idx, elm in enumerate(a):
+        if elm > 0:
+            output[idx] = heights[pos]
+            pos += 1
+    return output
 
 
-data = [-1, 150, 190, 170, -1, -1, 160, 180]
-print(sortByHeight(data))
+testData = [-1, 150, 190, 170, -1, -1, 160, 180]
+print(sortByHeight(testData))
--- mySubstringByLength.py
+++
@@ -1,9 +0,0 @@
-# https://app.codesignal.com/challenge/MbQ2B83RMHNtRwxZL
-# Implement an algorithm that can output a substring of the given string
-# given a starting point and a length.
-
-
-def mySubstringByLength(inputString, start, length):
-    return inputString[start:start+length]
-
-print(mySubstringByLength("abcde", 2, 3))
